af021c52c5b66d9ce046031a92791ace1e8f7c00
Jechf <jesuse.chf@gmail.com>
2022-06-27 23:16:14 +0000

    fix

--- src/js/component/cardgroup.js
+++ src/js/component/cardgroup.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Card } from './card'
 
-const imagenesCard = ["https://picsum.photos/200?random=1", "https://picsum.photos/200?random=2", "https://picsum.photos/200?random=3", "https://picsum.photos/200?random=4"]
+const cardImages = ["https://picsum.photos/200?random=1", "https://picsum.photos/200?random=2", "https://picsum.photos/200?random=3", "https://picsum.photos/200?random=4"]
+
+
 export const Cardgroup = () => {
 
   return (
     <div className='container'>
     <div className='card-group'>
-      {imagenesCard.map((img, i) => {
-        return <Card img={img} />
+      {cardImages.map((cardImage, i) => {
+        return <Card img={cardImage} />
       })}
     </div>
     </div>
